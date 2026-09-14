e5d73aaa795a19122adc459fe4d9b34673d86cff
Dracotoffee0130 <joe.ha0130@gmail.com>
2026-09-14 12:51:28 +0630

    first commit to student grade

---
+++ students.py
@@ -0,0 +1,7 @@
+name = input("Enter name")
+mark1 = int(input("Enter mark 1:"))
+mark2 = int(input("Enter mark 2:"))
+
+average = int((mark1 + mark2)/2)
+print(f'Name: {name}')
+print(f'Average: {average}')
